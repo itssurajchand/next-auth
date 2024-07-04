a46a08792fef88f3ffaab7df8ecd1b0c4422135f
Suraj <itssurajchand@gmail.com>
2024-07-04 11:53:12 +0530

    Update signin.js

--- src/pages/auth/signin.js
+++ src/pages/auth/signin.js
@@ -3,10 +3,17 @@
 import { signIn, useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
+import { useForm } from "react-hook-form";
 
 export default function SignIn() {
   const { data: session, status } = useSession();
   const router = useRouter();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
   useEffect(() => {
     // Redirect to profile page if user is logged in
@@ -15,7 +22,23 @@ export default function SignIn() {
     }
   }, [status]);
 
-  const handleSubmit = async (event) => {
+  const onSubmit = async (data) => {
+    const { email, password } = data;
+    const result = await signIn("credentials", {
+      redirect: false,
+      email,
+      password,
+    });
+    console.log(result, "result SignIn");
+    if (!result.error) {
+      // Redirect to dashboard or home page
+      router.push("/profile");
+    } else {
+      alert("User not exist");
+    }
+  };
+
+  /* const handleSubmit = async (event) => {
     event.preventDefault();
     const email = event.target.email.value;
     const password = event.target.password.value;
@@ -29,18 +52,21 @@ export default function SignIn() {
       // Redirect to dashboard or home page
       router.push("/profile");
     }
-  };
+  }; */
 
   return (
-    <div>
+    <div
+      style={{ display: "flex", alignItems: "center", flexDirection: "column" }}
+    >
       <h1>Sign In</h1>
-      <form onSubmit={handleSubmit}>
+      {/*  <form onSubmit={handleSubmit}>
         <input
           type="email"
           id="email"
           name="email"
           placeholder="Email"
           required
+          className="form-control"
         />
         <br />
         <input
@@ -52,7 +78,55 @@ export default function SignIn() {
         />
         <br />
         <button type="submit">Sign In</button>
-      </form>
+      </form> */}
+      <div className="App">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div>
+            <label>Email</label>
+            <input
+              type="text"
+              className="form-control"
+              name="email"
+              {...register("email", {
+                required: true,
+                pattern: /^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/,
+              })}
+            />
+            {errors.email && errors.email.type === "required" && (
+              <p className="errorMsg">Email is required.</p>
+            )}
+            {errors.email && errors.email.type === "pattern" && (
+              <p className="errorMsg">Email is not valid.</p>
+            )}
+          </div>
+          <div className="mt-2">
+            <label>Password</label>
+            <input
+              className="form-control"
+              type="password"
+              name="password"
+              {...register("password", {
+                required: true,
+                minLength: 6,
+              })}
+            />
+            {errors.password && errors.password.type === "required" && (
+              <p className="errorMsg">Password is required.</p>
+            )}
+            {errors.password && errors.password.type === "minLength" && (
+              <p className="errorMsg">
+                Password should be at-least 6 characters.
+              </p>
+            )}
+          </div>
+          <div>
+            <label></label>
+            <button type="submit" className="btn btn-primary mt-2">
+              Login
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
